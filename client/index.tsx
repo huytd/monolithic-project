9a4e66a6bfde33ee9b08da18b0a07f8b6e7f6c95
Huy Tran <kingbazoka@gmail.com>
2022-05-23 23:39:41 -0700

    Remove everything

--- client/index.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import { render } from 'react-dom';
-
-const App = () => {
-    return <div>Hello, World! This is a test!</div>;
-};
-
-render(<App/>, document.querySelector("#root"));
